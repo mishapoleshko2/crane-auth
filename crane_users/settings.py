8e640bdf494fb3748b8ab5e1555dc97f4e26fa1b
poleshkomi <poleshkomi@modeltech.ru>
2024-10-27 23:57:57 +0300

    Added settings object

--- crane_users/settings.py
+++ crane_users/settings.py
@@ -1,10 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import PostgresDsn
+from pydantic import PostgresDsn, Field
 
 class AppSettings(BaseSettings):
 	model_config = SettingsConfigDict(env_prefix='crane_users_', extra='ignore')
-	db_url: PostgresDsn
+	db_url: PostgresDsn = Field(default=...)
 
 	class Congfig:
 		env_file = '.env'
 		env_file_encoding = 'utf-8'
+
+
+settings = AppSettings()
