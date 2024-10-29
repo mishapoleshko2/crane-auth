c2f7088f45513f85eb5b09cfc1b824ad96c17a30
poleshkomi <poleshkomi@modeltech.ru>
2024-10-29 22:14:15 +0300

    renamed folder

---
+++ crane_users/infra/sqlalchemy_db/utils.py
@@ -0,0 +1,20 @@
+from typing import AsyncIterator
+from contextlib import asynccontextmanager
+
+from sqlalchemy.ext.asyncio.session import AsyncSession
+
+from crane_users.infra.sqlalchemy_db.base import async_session
+
+
+@asynccontextmanager
+async def get_session(with_commit: bool = False) -> AsyncIterator[AsyncSession]:
+    try:
+        async with async_session() as session:
+            yield session
+        if with_commit:
+            await session.commit()
+    except Exception as e:
+        await session.rollback()
+        raise e
+    finally:
+        await session.close()
