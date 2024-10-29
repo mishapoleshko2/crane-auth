2a78805799a5975c4bd83117318107ec2f78b569
poleshkomi <poleshkomi@modeltech.ru>
2024-10-29 22:09:22 +0300

    applyed ruff

--- crane_users/domain/user.py
+++ crane_users/domain/user.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, EmailStr, SecretStr
 
+
 class User(BaseModel):
-	id: int
-	email: EmailStr | None
-	pasword: SecretStr
-	company_id: int
+    id: int
+    email: EmailStr | None
+    pasword: SecretStr
+    company_id: int
