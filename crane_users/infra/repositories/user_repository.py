4a66f58b3f9fe29f11b9c5bd934f1dbca5293162
poleshkomi <poleshkomi@modeltech.ru>
2024-11-14 19:24:43 +0300

    expanded repository

--- crane_users/infra/repositories/user_repository.py
+++ crane_users/infra/repositories/user_repository.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from sqlalchemy import delete
+from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from crane_users.domain.entities.user import User
@@ -12,12 +12,19 @@ from crane_users.infra.sqlalchemy_db.models.user import User as DBUser
 class PostgresUserRepository(UserRepository):
     session: AsyncSession
 
-    async def find_user_by_email(self, email: str) -> User | None: ...
+    async def find_user_by_email(self, email: str) -> User | None:
+        query = select(DBUser).where(DBUser.email == email)
+        result = await self.session.execute(query)
+        user = result.scalar_one_or_none()
+        return user
 
-    async def find_user_by_login(self, login: str) -> User | None: ...
+    async def find_user_by_login(self, login: str) -> User | None:
+        query = select(DBUser).where(DBUser.login == login)
+        result = await self.session.execute(query)
+        user = result.scalar_one_or_none()
+        return user
 
     async def create_user(self, login: str, email: str, password_hash: str) -> User:
-        print(self.session)
         db_user = DBUser(login=login, email=email, password_hash=password_hash)
         self.session.add(db_user)
         await self.session.commit()
