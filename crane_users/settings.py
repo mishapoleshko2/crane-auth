32922d289de79e01b65495ecaa9f5389a784da34
poleshkomi <poleshkomi@modeltech.ru>
2024-12-07 14:12:51 +0300

    fixed settings for mypy

--- crane_users/settings.py
+++ crane_users/settings.py
@@ -1,10 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import PostgresDsn, Field
+from pydantic import PostgresDsn, SecretStr, Field
 
 
 class AppSettings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="crane_users_", extra="ignore")
     db_url: PostgresDsn = Field(default=...)
+    jwt_secret: SecretStr = Field(default=...)
+    access_token_ttl: int = Field(default=...)
+    refresh_token_ttl: int = Field(default=...)
+    jwt_encoding_algorithm: str = Field(default=...)
 
     class Congfig:
         env_file = ".env"
