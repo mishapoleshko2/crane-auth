a80346e1ae4c3e679876e9e3faf248e410669e1f
poleshkomi <poleshkomi@modeltech.ru>
2025-01-31 01:09:12 +0300

    added deleted exception

--- crane_auth/interactor/exceptions.py
+++ crane_auth/interactor/exceptions.py
@@ -6,7 +6,6 @@ class IncorrectPasswordError(SystemException):
         super().__init__("Incorrect login/email or password")
 
 
-
 class UserIsNotRegisteredException(SystemException):
     def __init__(self) -> None:
         super().__init__("User is not registered")
@@ -25,3 +24,7 @@ class RefreshSessionIsExpiredException(SystemException):
 class UserIsExistError(SystemException):
     def __init__(self) -> None:
         super().__init__("User with the same login/email is exist")
+
+class DTOValidationException(SystemException):
+	def __init__(self, validation_msg: str) -> None:
+		super().__init__(f"Invalid data: {validation_msg}")
